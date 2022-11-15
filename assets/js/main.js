3d395f701f30a42c83bf7262f849e8f009ef51c2
RaphaelMolina <manson1307@gmail.com>
2022-11-15 15:30:41 -0300

    Criação do Projeto.

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+
+const maxRecords = 151;
+const limit = 12;
+let offset = 0;
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        pokemonList.innerHTML += pokemons.map((pokemon) => 
+            `<li class="pokemon ${pokemon.type}" onclick="openModal(${pokemon.number})">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </li>`).join('');
+    });
+}
+
+loadPokemonItens(offset, limit);
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit;
+    const qtdRecordsWithNextPage = offset + limit;
+    
+    if (qtdRecordsWithNextPage > maxRecords) {
+        const newLimit = maxRecords - offset;
+        loadPokemonItens(offset, newLimit);
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
+        // Ou loadMoreButton.remove(loadMoreButton);
+    } else {
+        loadPokemonItens(offset, limit);
+    }
+});
---
+++ assets/js/pokemon-model.js
@@ -0,0 +1,18 @@
+class Pokemon {
+    number;
+    name;
+    type;
+    types = [];
+    photo;
+    abilities = [];
+    height;
+    weight;
+    stats = [];
+    totalStats;
+    speciesUrl;
+    species;
+    growthRate;
+    habitat;
+    eggGroups = [];
+    moves = [];
+}
